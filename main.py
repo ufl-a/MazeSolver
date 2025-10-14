e8f6982ff3d0b114cc8d20e8517d8e68bc32ac2e
ufl-a <a.khatiwada@ufl.edu>
2025-10-14 08:33:30 -0400

    wip

--- main.py
+++ main.py
@@ -12,9 +12,10 @@ class Maze:
         self.mid=(r//2,c//2)
         self.B=[[1 for i in range(0,c)] for j in range(0,r)]
         self.dirs=[(-1,0),(1,0),(0,-1),(0,1)] #LRDU
-    def put(self,*idxs,num): 
-        for idx in idxs:
-            self.B[idx[0]][idx[1]]=num
+
+    def __str__(self): return '\n'.join(str(self.B[r]) for r in range(self.R))
+    def put(self,idx,num): 
+        self.B[idx[0]][idx[1]]=num
     def sum2(self,t0,t1):return (t0[0]+t1[0],t0[1]+t1[1])
     def rand(self, n):
         ret=[]
@@ -27,7 +28,7 @@ class Maze:
 
     def djik(self,start,end): #0:empty tile, 1: blocked; -1: target
         #self.put((self.C-1,self.r-1),1)
-        self.put(self.mid,'x'); self.put(end,-1); #self.M.print()
+        self.put(self.mid,'x'); 
         heap,visited=[],set() 
         heapq.heappush(heap,(0,start,[start])) #key:(cost,pos,path)
         while heap:
@@ -35,11 +36,10 @@ class Maze:
             for D in self.dirs: 
                 r,c=R+D[0],C+D[1] 
                 if not (0<=r<self.R and 0<=c<self.C) or self.B[r][c] or (r,c) in visited: continue
-                if (R_,C_)==end: return cost,path+[end]
-                visited.add((R_,C_))
-                heapq.heappush(heap,(cost+1,(R_,C_),path+[(R_,C_)]))
-        return -1
-    def __str__(self): return '\n'.join(str(self.B[r]) for r in range(self.R))
+                if (r,c)==end: return cost,path+[end]
+                visited.add((r,c))
+                heapq.heappush(heap,(cost+1,(r,c),path+[(r,c)]))
+        return -1,[]
 
     def ns(self,n): return [(a+c, b+d) for ((a,b),(c,d)) in list(zip(self.dirs, [n]*4))] #neighbors
     def fs(self,n,fs): 
@@ -57,55 +57,56 @@ class Maze:
 
 
     def map(self,p,f): #prims algo,p=path
+        path={p:None}
         self.B[p[0]][p[1]]=0 #gen from here 
         fil=lambda rc:(0<=rc[0]<self.R and 0<=rc[1]<self.C) and not self.B[rc[0]][rc[1]]
         while(len(f)):
             fc=f.pop(random.randint(0,len(f)-1))
             #print(list(filter(fil,self.ns(fc))))
             #if ((fc[0] in (0,self.R-1)) or (fc[1] in (0,self.C-1))): break #to bounds, too soon
-            if len(list(filter(fil,self.ns(fc))))==1:#dig if ex 1 neighboring dug tile 
+            if len(dug:=(list(filter(fil,self.ns(fc)))))==1:#dig if ex 1 neighboring dug tile 
                 self.B[fc[0]][fc[1]]=0;
+                path[(fc[0],fc[1])]=dug[0]
                 for _ in self.fns(fc,f):f.append(_)
+        path_=path.keys()
 
         side=random.randint(0,3)
         match side:
             case 0: 
-                while(self.B[1][(s:=random.randint(0,self.C-1))]!=0):s+=1
+                while(self.B[0][(s:=random.randint(0,self.C-1))]!=0):s+=1
                 s=(0,s)
             case 1: 
-                while(self.B[s:=random.randint(0,self.R-1)][-2]!=0):s+=1
+                while(self.B[s:=random.randint(0,self.R-1)][-1]!=0):s+=1
                 s=(s,self.C-1)
             case 2: 
-                while(self.B[-2][(s:=random.randint(0,self.R-1))])!=0:s+=1
+                while(self.B[-1][(s:=random.randint(0,self.R-1))])!=0:s+=1
                 s=(self.R-1,s)
             case 3: 
-                while(self.B[s:=random.randint(0,self.R-1)][1]!=0):s+=1
+                while(self.B[s:=random.randint(0,self.R-1)][0]!=0):s+=1
                 s=(s,0)
-        for _ in range(self.C):
-            self.B[0][_]=1
-            self.B[-1][_]=1
-        for _ in range(self.R):
-            self.B[_][0]=1
-            self.B[_][-1]=1
-        self.B[s[0]][s[1]]=1
-        
 
-        return p 
+        ret,r=[],path[s]
+        ret.append(s)
+        ret.append(r)
+        while r!=None:ret.append(r:=path[r])
+        return s,ret
 
 
-dims=(20,50)
-M = Maze(*dims)#print(M.fns(M.mid,()))
-M.map(M.mid,M.fns(M.mid,()))
-print(M)
 
 def run():
     app = Flask(__name__)
     @app.route("/")
     def home():
+        dims=(20,50)
+        M=Maze(*dims)#;print(M.fns(M.mid,()))
+        F,P=M.map(M.mid,M.fns(M.mid,()))
+        P0=M.djik(M.mid,F)[1]
         html=''
         for r in range(M.R):
-            #for c in range(M.C): html+=f'''<c style="color:{["black","red"][(r,c) in P]}">{M.B[r][c]}</c>'''
-            for c in range(M.C): html+=f'''<c>{M.B[r][c]}</c>'''
+            for c in range(M.C): html+=f'''<c style="color:{["black","red"][(r,c) in P]}">{M.B[r][c]}</c>'''
+            #for c in range(M.C): html+=f'''<c>{M.B[r][c]}</c>'''
             html+='<br>'
         return html
     app.run(debug=True)
+run()
+
